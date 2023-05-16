a085cae1ecdebe19aa7212a3f518fdfcb264588d
Nursyamsu Rijal Usman <119686454+IJ4L@users.noreply.github.com>
2023-05-16 16:38:08 +0800

    update ui

--- lib/ui/pages/diagnosis_value_page.dart
+++ lib/ui/pages/diagnosis_value_page.dart
@@ -33,14 +33,62 @@ class Diagnosisvalue extends StatelessWidget {
             ),
             Positioned(
               top: 10.h,
-              child: Container(
-                height: 180.h,
-                width: MediaQuery.of(context).size.width - 32.w,
-                margin: EdgeInsets.symmetric(horizontal: 16.w),
-                decoration: BoxDecoration(
-                  color: kBlackColor,
-                  borderRadius: BorderRadius.circular(20.r),
-                ),
+              child: Column(
+                children: [
+                  Container(
+                    height: 180.h,
+                    width: MediaQuery.of(context).size.width - 32.w,
+                    margin: EdgeInsets.only(
+                      right: 16.w,
+                      left: 16.w,
+                      bottom: 25.h,
+                    ),
+                    decoration: BoxDecoration(
+                      color: kSecondColor,
+                      borderRadius: BorderRadius.circular(20.r),
+                    ),
+                  ),
+                  Text(
+                    'Karat Daun\n(Hemilia Vasatrix)',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 18.sp,
+                      fontWeight: semiBold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  Container(
+                    height: 39.h,
+                    width: 310.w,
+                    margin: EdgeInsets.symmetric(vertical: 25.h),
+                    padding: EdgeInsets.symmetric(horizontal: 16.w),
+                    decoration: BoxDecoration(
+                      color: kPrimaryColor,
+                      borderRadius: BorderRadius.circular(50.r),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Solusi',
+                          style: blackTextStyle,
+                        ),
+                        const Icon(Icons.arrow_drop_down)
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: 310.w,
+                    padding: EdgeInsets.all(22.r),
+                    decoration: BoxDecoration(
+                      color: kPrimaryColor,
+                      borderRadius: BorderRadius.circular(20.r),
+                    ),
+                    child: Text(
+                      'Lorem ipsum dolor sit amet consectetur. Metus donec interdum enim sem id montes. Risus malesuada feugiat amet mi dignissim. Viverra pellentesque ut arcu rhoncus eu. Ac morbi nunc arcu suspendisse lacinia. Vivamus vulputate hac vel velit at facilisis lectus at diam. Lorem habitant eu.',
+                      style: blackTextStyle,
+                    ),
+                  )
+                ],
               ),
             ),
           ],
